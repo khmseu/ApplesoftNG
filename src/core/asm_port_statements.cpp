7a3fbd93b1908b606bdb5f93f3442ea2875e0b61
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:31:20 +0200

    refactor(core): remove redundant MON_RD2BIT declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -21,7 +21,6 @@ namespace applesoft::asm_port {
 constexpr std::size_t kTokenCount = 107;
 constexpr std::uint8_t kTokenBase = 0x80u;
 
-void MON_RD2BIT();
 void MON_HEADR(std::uint8_t delay_code);
 bool MON_RDBIT();
 std::uint8_t MON_RDBYTE();
