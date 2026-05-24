ae367251e1f0e5e7f41c4f666530078c22de10ca
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:35:29 +0200

    refactor(core): remove redundant AS_PTRGET declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -21,7 +21,6 @@ namespace applesoft::asm_port {
 constexpr std::size_t kTokenCount = 107;
 constexpr std::uint8_t kTokenBase = 0x80u;
 
-std::uint16_t AS_PTRGET();
 void AS_FRMEVL();
 bool AS_CHKVAL(std::uint8_t savedValTyp);
 void AS_ROUND_FAC();
