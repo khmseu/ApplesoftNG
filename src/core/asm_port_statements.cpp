04aece004a8b7887ffc488d705a686d16d35ff75
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:35:47 +0200

    refactor(core): remove redundant AS_FRMEVL declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -21,7 +21,6 @@ namespace applesoft::asm_port {
 constexpr std::size_t kTokenCount = 107;
 constexpr std::uint8_t kTokenBase = 0x80u;
 
-void AS_FRMEVL();
 bool AS_CHKVAL(std::uint8_t savedValTyp);
 void AS_ROUND_FAC();
 void AS_AYINT();
