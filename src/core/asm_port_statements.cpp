da2a231066aa7eaf3e70e1c0bbd0a277ce2b51e7
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:32:46 +0200

    refactor(core): remove redundant MON_NXTA1 declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -21,7 +21,6 @@ namespace applesoft::asm_port {
 constexpr std::size_t kTokenCount = 107;
 constexpr std::uint8_t kTokenBase = 0x80u;
 
-bool MON_NXTA1();
 void MON_BELL();
 void MON_PRERR();
 void MON_RESTORE();
