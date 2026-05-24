8ec06c4c20d8e0b0de88550eca84dc2c4f9c5241
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:35:10 +0200

    refactor(core): remove redundant AS_PROGIO declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -21,7 +21,6 @@ namespace applesoft::asm_port {
 constexpr std::size_t kTokenCount = 107;
 constexpr std::uint8_t kTokenBase = 0x80u;
 
-void AS_PROGIO();
 std::uint16_t AS_PTRGET();
 void AS_FRMEVL();
 bool AS_CHKVAL(std::uint8_t savedValTyp);
