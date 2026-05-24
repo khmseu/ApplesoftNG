30cdf485414f67a542e773f3d3078b1beb8a12b7
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:30:25 +0200

    refactor(core): remove redundant MON_READ declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -21,7 +21,6 @@ namespace applesoft::asm_port {
 constexpr std::size_t kTokenCount = 107;
 constexpr std::uint8_t kTokenBase = 0x80u;
 
-void MON_READ();
 void MON_RD2();
 void MON_RD3();
 void MON_RD2BIT();
