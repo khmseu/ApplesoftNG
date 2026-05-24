238e6de79846d44d676c7c375bd8a99e3f2dcbdc
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:40:08 +0200

    refactor(core): remove redundant AS_FL1 declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -21,7 +21,6 @@ namespace applesoft::asm_port {
 constexpr std::size_t kTokenCount = 107;
 constexpr std::uint8_t kTokenBase = 0x80u;
 
-bool AS_FL1(std::uint16_t startAddress);
 bool AS_NEW_impl();
 void AS_SCRTCH_impl();
 bool AS_SETPTRS_impl();
