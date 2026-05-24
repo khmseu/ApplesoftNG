40276a5aaed9d0fef1dfa54ddd1dfc6ace934860
Kai Henningsen <kai.extern+git@gmail.com>
2026-05-24 03:42:29 +0200

    refactor(core): remove redundant AS_L_RECALL_1 declaration

--- src/core/asm_port_statements.cpp
+++ src/core/asm_port_statements.cpp
@@ -24,7 +24,6 @@ constexpr std::uint8_t kTokenBase = 0x80u;
 bool AS_CLEAR_impl();
 void AS_CLEARC_impl();
 void AS_STXTPT_impl();
-void AS_L_RECALL_1();
 void AS_GETARY2();
 void AS_GETARYPT();
 void AS_TAPEPNT();
